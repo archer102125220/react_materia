70c7f71f40ba8f431dabbef50840cdab0ab5b15d
Archer <archer102125220.2015@gmail.com>
2020-03-06 22:17:06 +0800

    update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,16 @@ module.exports = {
       exclude: /node_modules/
     },
     { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader' },
-    { test: /\.(jpe?g|png|gif|svg|png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
+    {
+      test: /\.(jpe?g|png|gif|svg|png|woff|woff2|eot|ttf|svg)$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: { limit: 100000 }
+        },
+        'image-webpack-loader'
+      ]
+    },
     {
       test: /\.scss$/,
       use: ['style-loader', 'css-loader', 'sass-loader']
